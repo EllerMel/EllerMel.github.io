f58698e5e953f6136e1244e0ef004872687e87c0
M Eller <msmaedev@gmail.com>
2019-03-19 15:24:43 -0400

    VueJS Clock!

--- js/main.js
+++ js/main.js
@@ -14,6 +14,69 @@ Vue.component('date', {
     template: `<h3>{{ formatDate }}</h3>`
 })
 
+Vue.component('clock', {
+    data: function () {
+        return { currentTime: new Date() }
+    },
+    methods: {
+        displayTime() {
+            var self = this;
+            if (!this.interval) {
+                this.interval = setInterval(() => {
+                    self.currentTime = new Date();
+                    self.time_str = self.getStandardTime;
+                }, 100);
+            } else {
+                clearInterval(this.interval);
+                this.interval = 0;
+            }
+        }
+    },
+    created() {
+        this.displayTime();
+    },
+    computed: {
+        standardHrs() {
+            if (this.currentTime.getHours() >= 13) {
+                return this.currentTime.getHours() - 12
+            }
+            else if (this.currentTime.getHours() == 0) {
+                return 12
+            }
+            else {
+                return this.currentTime.getHours()
+            }
+        },
+        meridiem() {
+            if (this.currentTime.getHours() >= 12) {
+                return "PM"
+            }
+            else if (this.currentTime.getHours() >= 0 && this.currentTime.getHours() < 12) {
+                return "AM"
+            }
+        },
+        mins() {
+            if (this.currentTime.getMinutes() < 10) {
+                return '0' + this.currentTime.getMinutes()
+            }
+            else return this.currentTime.getMinutes()
+        },
+        sec() {
+            if (this.currentTime.getSeconds() < 10) {
+                return '0' + this.currentTime.getSeconds()
+            }
+            else return this.currentTime.getSeconds()
+        },
+        getStandardTime() {
+            //return this.standardHrs + ":" + this.mins + ":" + this.sec + " " + this.meridiem
+            //if adding back in seconds, update the interval to 1000
+            return this.standardHrs + ":" + this.mins + " " + this.meridiem
+        }
+    },
+    template: `<h3>{{ getStandardTime }}</h3>`
+})
+
+
 var app = new Vue({
     el: '#app'
 })
